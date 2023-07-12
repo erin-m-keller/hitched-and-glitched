14d0eab3cd61641f38b5a68fa916f95fe54bcb95
Adrena Lewis <74272608+x3n0g3n@users.noreply.github.com>
2023-07-12 18:41:15 -0400

    Homepage attempt

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
+// const Home = () => {
+//   return (
+//     <>
+//     <div className="main-content">
+//       <h1>Home</h1>
+//     </div>
+//     </>
+//   );
+// };
+
+// export default Home;
+
 const Home = () => {
   return (
-    <>
-    <div className="main-content">
-      <h1>Home</h1>
-    </div>
-    </>
-  );
+  <div>
+  <h1>Hitched & G-G-G-Glitched</h1>
+    <h2>Calm your motherboard!</h2>
+      <p> My name is B-B-Belle. I have been imported here to help you not stress about the HOPEfully happiest day of your life. This site holds many g-goodies to help you organize and navigate your thoughts so you dont b-b-burn your circuits</p>
+        <img src="robot.png" width="200px"></img>
+        
+</div>
+);
 };
+ReactDom.render(<Home/>,getElementById("root") );
+
+// login button to be found on the home page
+
+// create account button found on the home page
+
 
 export default Home;
