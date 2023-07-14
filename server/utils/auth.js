f21bbd5c1312fdf37a70acb2a916f3ed79c3ad54
Erin Keller <aestheticartist@gmail.com>
2023-07-14 15:27:59 -0400

    removed console log

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,29 +1,23 @@
 // initialize variables
-const jwt = require("jsonwebtoken"),
-      secret = process.env.SECRET,
-      expiration = "5h";
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET;
+const expiration = "5h";
 
 module.exports = {
-  // function to sign a token with the provided payload, secret, and expiration
-  generateAuthToken: ({ username, email, _id }) => 
-    jwt.sign({ data: { username, email, _id } }, secret, { expiresIn: expiration }), 
-  // function to extract and verify a token from the request, assigning the decoded 
-  // user data to the req.user property if the token is valid
+  generateAuthToken: ({ username, email, _id }) => jwt.sign({ data: { username, email, _id } }, secret, { expiresIn: expiration }),
   authMiddleware: ({ req }) => {
-    // initialize variables
     let token = req.body.token || req.query.token || req.headers.authorization;
-    // extract the token from the authorization header if present
-    if (req.headers.authorization) token = token.split(" ").pop().trim();
-    // if token is not found, return the request object as is 
-    if (!token) return req; 
-    try {
-      // verify and decode the token, and assign the decoded data to the 'user' property of the request object
-      req.user = jwt.verify(token, secret, { maxAge: expiration }).data; 
-    } catch {
-      // log an error message if the token is invalid
-      console.log("Token is invalid."); 
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
     }
-    // return the request object
-    return req; 
+    if (!token) {
+      return req;
+    }
+    try {
+      req.user = jwt.verify(token, secret);
+    } catch (err) {
+      //console.log("Token verification error:", err.message);
+    }
+    return req;
   },
 };
