327e73272c21d3613afc3c7fdae0348a9f9a1d9d
Dallen Hatton <Dhatton2001@gmail.com>
2023-07-17 21:00:53 -0400

    restrutire, import and function

--- client/src/pages/Countdown.js
+++ client/src/pages/Countdown.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Input, Card, notification, Empty } from 'antd';
 import Auth from '../utils/auth';
 
-// Function to validate the date format (YYYY-MM-DD)
+function CountdownPage() {
+  const [targetDate, setTargetDate] = useState('');
+  const [countdownCompleted, setCountdownCompleted] = useState(false);
+
+/*/ Function to validate the date format (YYYY-MM-DD)
 function isValidDate(dateString) {
     var regEx = /^\d{4}-\d{2}-\d{2}$/;
     if (!dateString.match(regEx)) return false; // Invalid format
