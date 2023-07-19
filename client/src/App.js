6ca26b37ad475eace1584f6f7f1dc86ee63fe1cf
Dallen Hatton <Dhatton2001@gmail.com>
2023-07-19 18:51:22 -0400

    add countdown page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// Import the necessary dependencies and components
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
@@ -6,28 +5,9 @@ import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Inspiration from './pages/Inspiration';
 import Navbar from './components/Navbar';
-import Countdown from './pages/Countdown'; 
+import CountdownPage from './CountdownPage'; // Import the CountdownPage component
 
-
-// Create a new in-memory cache for Apollo Client
-const cache = new InMemoryCache();
-
-// Configure the Apollo Client
-const client = new ApolloClient({
-  request: operation => {
-    // Retrieve JWT token from localStorage
-    const token = localStorage.getItem('jwtToken');
-    // Set the "authorization" header for GraphQL requests
-    operation.setContext({
-      headers: { authorization: token ? `Bearer ${token}` : '' }
-    });
-  },
-  uri: '/graphql', // URI for the GraphQL server
-  cache // Use the created in-memory cache
-});
-
-// Clear the Apollo Client store
-client.clearStore();
+// ... (Apollo Client configuration and other code)
 
 function App() {
   return (
@@ -45,8 +25,8 @@ function App() {
             <Route exact path='/dashboard' component={Dashboard} />
             {/* Route for the Inspiration page */}
             <Route exact path='/inspiration' component={Inspiration} />
-            {/* Route for countdown page*/}
-            <Route exact path='/countdown' component={Countdown} />
+            {/* Route for the countdown page */}
+            <Route exact path='/countdown' component={CountdownPage} />
             {/* If no matching route is found, render this */}
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
